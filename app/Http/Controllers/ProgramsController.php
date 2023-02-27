28e066a53e3df2e2d99c370a7c8fc5fc093310f9
Andi Dwi Prastyo <89091156+andidprastyo@users.noreply.github.com>
2023-02-27 13:57:13 +0700

    Add Comment

--- app/Http/Controllers/ProgramsController.php
+++ app/Http/Controllers/ProgramsController.php
@@ -6,14 +6,17 @@ use Illuminate\Http\Request;
 
 class ProgramsController extends Controller
 {
+    // Ke Karir
     public function karir(){
         return redirect('<a href=https://www.educastudio.com/program/karir>Karir</link>');
     }
 
+    // Ke Magang
     public function magang(){
         return redirect('<a href=https://www.educastudio.com/program/magang>Magang</link>');
     }
 
+    // Ke Kunjungan Industri
     public function kunjungan_industri(){
         return redirect('<a href=https://www.educastudio.com/program/kunjungan-industri>Kunjungan Industri</link>');
     }
