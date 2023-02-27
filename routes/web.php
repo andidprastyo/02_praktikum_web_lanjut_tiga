33f89bb3c3c5ab38e03cc5cc2c5ff0f7ae52b49c
Andi Dwi Prastyo <89091156+andidprastyo@users.noreply.github.com>
2023-02-27 14:17:47 +0700

    Add NewsController Directory

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactUsController;
 use App\Http\Controllers\ProgramsController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
